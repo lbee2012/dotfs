e15c9c93853b3e497d0c677f270d1076bb371562
lbee2012 <hoangflong2012@gmail.com>
2025-06-06 23:52:24 +0700

    adjusting

--- fish/config.fish
+++ fish/config.fish
@@ -3,9 +3,18 @@ if status is-interactive
 
 end
 
+set -g fish_greeting "
+Welcome, Master.Lbee"
+
 alias i3='startx'
 
-# IBus for GTK/Qt
+#fcitx5
+#set -x GTK_IM_MODULE fcitx
+#set -x QT_IM_MODULE fcitx
+#set -x XMODIFIERS "@im=fcitx"
+#set -x INPUT_METHOD fcitx
+
+#ibus
 set -x GTK_IM_MODULE   ibus
 set -x QT_IM_MODULE    ibus
 set -x XMODIFIERS      @im=ibus

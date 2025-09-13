1d918ff9151306850d422f9799e48412c80b63e3
lbee2012 <hoangflong2012@gmail.com>
2025-09-13 23:30:11 +0700

    grgrgr

--- fish/config.fish
+++ fish/config.fish
@@ -8,16 +8,19 @@ eveklyna."
 
 alias i3='startx'
 
+alias git.dotfs='cd /home/sata/Database/Git/dotfs'
+alias git.archy='cd /home/sata/Database/Git/archy'
+
 #fcitx5
-set -x GTK_IM_MODULE fcitx
-set -x QT_IM_MODULE fcitx
-set -x XMODIFIERS "@im=fcitx"
-set -x INPUT_METHOD fcitx
+#set -x GTK_IM_MODULE fcitx
+#set -x QT_IM_MODULE fcitx
+#set -x XMODIFIERS "@im=fcitx"
+#set -x INPUT_METHOD fcitx
 
 #ibus
-#set -x GTK_IM_MODULE   ibus
-#set -x QT_IM_MODULE    ibus
-#set -x XMODIFIERS      @im=ibus
+set -x GTK_IM_MODULE   ibus
+set -x QT_IM_MODULE    ibus
+set -x XMODIFIERS      @im=ibus
 
 export EDITOR=nvim
 export VISUAL=nvim
